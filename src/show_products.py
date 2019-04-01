268b1b01b487f6ac92615d653c4a155c605d3757
Trouble Lie <troubleli233@gmail.com>
2019-04-01 09:15:48 +0000

    add kmean-clustering

--- src/show_products.py
+++ src/show_products.py
@@ -5,19 +5,6 @@ import numpy as np
 import csv
 import os
 
-
-def read_csv(file_name, delimiter=','):
-    products = csv.reader(open(file_name, newline=''), delimiter=delimiter, quotechar='|')
-    my_list = []
-    for row in products:
-        dozen = []
-        for item in row:
-            if item is not '':
-                dozen.append(float(item))
-        my_list.append(dozen)
-    return my_list
-
-
 def where(benchmark, your_list):
     my_list = []
     for index, item in enumerate(your_list):
