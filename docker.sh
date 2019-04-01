bdb54fa98741e0ec61a817a606f4b8565c87814a
Trouble Lie <troubleli233@gmail.com>
2019-04-02 02:48:36 +0800

    reorder the setup procedure

--- docker.sh
+++ docker.sh
@@ -2,4 +2,4 @@
 #docker volume create trouble
 #docker volume inspect trouble
 #docker rm $(docker ps -aq)
-docker run --runtime=nvidia -it --mount type=volume,source=trouble,target=/notebooks -p 8888:8888 trouble/tensorflow:1.12.0-gpu-py3-git bash
+docker run --runtime=nvidia -it --name tensorflow_0112 --mount type=volume,source=trouble,target=/home/trouble -p 8888:8888 trouble/tensorflow:1.12.0-gpu-py3-git bash
--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker cp ~/docker_ws/ml-rrt/. tensorflow_0112:/home/trouble/ml-rrt
+docker cp ~/ml-rrt/. tensorflow_0112:/home/trouble/ml-rrt
 
 docker start tensorflow_0112 && docker exec -i tensorflow_0112 bash -c "cd /home/trouble/ml-rrt/src && python cluster.py" && docker stop tensorflow_0112
 
