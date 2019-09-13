68cc38bf2ead7e77be9446ff3a14650f40191a87
Trouble Lie <troubleli233@gmail.com>
2019-09-13 23:27:07 +0800

    add weights to copy.sh

--- copy.sh
+++ copy.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 docker cp ./src/. tensorflow_0112:/home/trouble/deepway/src
 docker cp ./dataset/. tensorflow_0112:/home/trouble/deepway/dataset
+docker cp ./dataset/. tensorflow_0112:/home/trouble/deepway/weights
