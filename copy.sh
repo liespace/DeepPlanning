54bea8483b0c055c13a40d7f7e1b7d2e7df9c030
Trouble Lie <troubleli233@gmail.com>
2019-09-14 20:53:24 +0800

    fix copy.sh

--- copy.sh
+++ copy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker cp ./src/. tensorflow_0112:/home/trouble/deepway/src
 docker cp ./dataset/. tensorflow_0112:/home/trouble/deepway/dataset
-docker cp ./dataset/. tensorflow_0112:/home/trouble/deepway/weights
+docker cp ./weights/. tensorflow_0112:/home/trouble/deepway/weights
