0d41a66f2b2045a191d93bfd22ca03be3b9f5854
Trouble Lie <troubleli233@gmail.com>
2019-09-24 01:24:43 +0800

    change run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-# export CUDA_VISIBLE_DEVICES=1
-docker start tensorflow_0112 && docker exec -i tensorflow_0112 bash -c "cd /home/trouble/deepway/ && python ./src/deepway.py" && docker stop tensorflow_0112
-
+docker start tensorflow_0112 && docker exec -i tensorflow_0112 bash -c "export CUDA_VISIBLE_DEVICES=1 && cd /home/trouble/deepway/ && python ./src/deepway.py" && docker stop tensorflow_0112
 #docker cp tensorflow_0112:/ml-rrt/. ~/docker_ws/ml-rrt
